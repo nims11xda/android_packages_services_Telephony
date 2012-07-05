342c1e94549a2ebe406e9c3421fac7b80f670091
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-07-05 17:38:17 +0200

    noice supp

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -521,6 +521,12 @@ public class CallFeaturesSetting extends PreferenceActivity
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.NOISE_SUPPRESSION, nsp);
             return true;
+        } else if (preference == mButtonNoiseSuppression) {
+            int nsp = mButtonNoiseSuppression.isChecked() ? 1 : 0;
+            // Update Noise suppression value in Settings database
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.NOISE_SUPPRESSION, nsp);
+            return true;
         } else if (preference == mButtonAutoRetry) {
             android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
                     android.provider.Settings.Global.CALL_AUTO_RETRY,
@@ -1690,6 +1696,15 @@ public class CallFeaturesSetting extends PreferenceActivity
             int flipAction = Settings.System.getInt(getContentResolver(),
                     Settings.System.FLIP_ACTION, 0);
             mFlipAction.setDefaultValue(String.valueOf(flipAction));
+        }
+
+        if (mButtonNoiseSuppression != null) {
+            if (getResources().getBoolean(R.bool.has_in_call_noise_suppression)) {
+                mButtonNoiseSuppression.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mButtonNoiseSuppression);
+                mButtonNoiseSuppression = null;
+            }
         }
 
         if (!getResources().getBoolean(R.bool.world_phone)) {
