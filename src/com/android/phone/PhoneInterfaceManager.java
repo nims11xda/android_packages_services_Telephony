c86459d551715bab3e9bbd1368dfdbddb29fc1cb
maxwen <max.weninger@gmail.com>
2013-10-30 12:12:07 -0200

    wake

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -32,6 +32,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.os.UserHandle;
 import android.telephony.NeighboringCellInfo;
 import android.telephony.CellInfo;
@@ -67,6 +68,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
     private static final int CMD_TOGGLE_STATE = 7;
+    private static final int CMD_TOGGLE_2G = 7; // used in screen action implementation
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -305,6 +307,20 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                 mMainThreadHandler.obtainMessage(CMD_TOGGLE_STATE));
         android.provider.Settings.Global.putInt(mApp.getContentResolver(),
                 android.provider.Settings.Global.PREFERRED_NETWORK_MODE, networkStatus);
+    }
+
+    // Used in Screen action implementation
+    public void toggle2G(boolean on) {
+        int network = -1;
+        if (on) {
+            network = PhoneConstants.NT_MODE_GSM_ONLY;
+        } else {
+            network = PhoneConstants.NT_MODE_WCDMA_PREF;
+        }
+        mPhone.setPreferredNetworkType(network,
+                mMainThreadHandler.obtainMessage(CMD_TOGGLE_2G));
+        Settings.Secure.putInt(mApp.getContentResolver(),
+                Settings.Global.PREFERRED_NETWORK_MODE, network);
     }
 
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
