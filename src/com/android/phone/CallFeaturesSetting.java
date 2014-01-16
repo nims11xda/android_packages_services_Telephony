40fd34bb703f4efb90304224de03d121c064ef1d
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-01-16 19:21:35 +0700

    Telephony: fix Flip to mute/silent

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -300,6 +300,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         CommandsInterface.CF_REASON_NO_REPLY,
         CommandsInterface.CF_REASON_NOT_REACHABLE
     };
+
     private static final CharSequence FLIP_ACTION_KEY = "flip_action";
 
     private class VoiceMailProviderSettings {
@@ -609,6 +610,9 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mButtonSipCallOptions) {
             handleSipCallOptionsChange(objValue);
         }else if (preference == mFlipAction) {
+            int i = Integer.parseInt((String) objValue);
+            Settings.System.putInt(mPhone.getContext().getContentResolver(), Settings.System.FLIP_ACTION_KEY,
+                    i);
             updateFlipActionSummary((String) objValue);
         }
         // always let the preference setting proceed.
@@ -620,8 +624,6 @@ public class CallFeaturesSetting extends PreferenceActivity
         if (mFlipAction != null) {
             String[] summaries = getResources().getStringArray(R.array.flip_action_summary_entries);
             mFlipAction.setSummary(getString(R.string.flip_action_summary, summaries[i]));
-            Settings.System.putInt(getContentResolver(), Settings.System.FLIP_ACTION_KEY,
-                    i);
         }
     }
 
@@ -1612,7 +1614,6 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mButtonHAC != null) {
             if (getResources().getBoolean(R.bool.hac_enabled)) {
-
                 mButtonHAC.setOnPreferenceChangeListener(this);
             } else {
                 prefSet.removePreference(mButtonHAC);
@@ -1631,9 +1632,9 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mFlipAction != null) {
             mFlipAction.setOnPreferenceChangeListener(this);
-            int flipAction = Settings.System.getInt(getContentResolver(),
+            int flipAction = Settings.System.getInt(mPhone.getContext().getContentResolver(),
                     Settings.System.FLIP_ACTION_KEY, 0);
-            mFlipAction.setDefaultValue(String.valueOf(flipAction));
+            mFlipAction.setValue(Integer.toString(flipAction));
         }
 
         if (!getResources().getBoolean(R.bool.world_phone)) {
@@ -1864,6 +1865,9 @@ public class CallFeaturesSetting extends PreferenceActivity
         }
 
         if (mFlipAction != null) {
+            int flipAction = Settings.System.getInt(getContentResolver(),
+                    Settings.System.FLIP_ACTION_KEY, 0);
+            mFlipAction.setValue(Integer.toString(flipAction));
             updateFlipActionSummary(mFlipAction.getValue());
         }
 
